d685c7ff4b99fe88e9c41deb91ef342af8bc9568
Nunbin <jpsp328@gmail.com>
2025-06-07 15:48:43 +0900

    다시 지우고 프로젝트 복사함

---
+++ KioskProject/KioskProject/UI/PaymentUI.Designer.cs
@@ -0,0 +1,194 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Payment_UI
+{
+    partial class PaymentUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ChangePaymentMethod_btn = new System.Windows.Forms.Button();
+            this.PerPersonAmount = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.CancelPaying_btn = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelCount = new System.Windows.Forms.Label();
+            this.Plus_btn = new System.Windows.Forms.Button();
+            this.Minus_btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ChangePaymentMethod_btn
+            // 
+            this.ChangePaymentMethod_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ChangePaymentMethod_btn.Font = new System.Drawing.Font("맑은 고딕", 14F);
+            this.ChangePaymentMethod_btn.Location = new System.Drawing.Point(299, 804);
+            this.ChangePaymentMethod_btn.Name = "ChangePaymentMethod_btn";
+            this.ChangePaymentMethod_btn.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.ChangePaymentMethod_btn.Size = new System.Drawing.Size(145, 42);
+            this.ChangePaymentMethod_btn.TabIndex = 40;
+            this.ChangePaymentMethod_btn.Text = "결제수단 변경";
+            this.ChangePaymentMethod_btn.UseVisualStyleBackColor = true;
+            this.ChangePaymentMethod_btn.Click += new System.EventHandler(this.ChangePaymentMethod_btn_Click);
+            // 
+            // PerPersonAmount
+            // 
+            this.PerPersonAmount.AutoSize = true;
+            this.PerPersonAmount.Cursor = System.Windows.Forms.Cursors.No;
+            this.PerPersonAmount.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.PerPersonAmount.Font = new System.Drawing.Font("맑은 고딕", 14F);
+            this.PerPersonAmount.Location = new System.Drawing.Point(486, 21);
+            this.PerPersonAmount.Name = "PerPersonAmount";
+            this.PerPersonAmount.Size = new System.Drawing.Size(63, 25);
+            this.PerPersonAmount.TabIndex = 39;
+            this.PerPersonAmount.Text = "label6";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 59);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(560, 724);
+            this.flowLayoutPanel1.TabIndex = 38;
+            // 
+            // CancelPaying_btn
+            // 
+            this.CancelPaying_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CancelPaying_btn.Font = new System.Drawing.Font("맑은 고딕", 14F);
+            this.CancelPaying_btn.Location = new System.Drawing.Point(450, 804);
+            this.CancelPaying_btn.Name = "CancelPaying_btn";
+            this.CancelPaying_btn.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.CancelPaying_btn.Size = new System.Drawing.Size(112, 42);
+            this.CancelPaying_btn.TabIndex = 36;
+            this.CancelPaying_btn.Text = "결제 취소";
+            this.CancelPaying_btn.UseVisualStyleBackColor = true;
+            this.CancelPaying_btn.Click += new System.EventHandler(this.CancelPaying_btn_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("맑은 고딕", 14F);
+            this.button3.Location = new System.Drawing.Point(15, 804);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(195, 42);
+            this.button3.TabIndex = 35;
+            this.button3.Text = "포인트 조회 및 사용";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("맑은 고딕", 14F);
+            this.label3.Location = new System.Drawing.Point(326, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 25);
+            this.label3.TabIndex = 34;
+            this.label3.Text = "1인당 결제금액 :";
+            // 
+            // labelCount
+            // 
+            this.labelCount.AutoSize = true;
+            this.labelCount.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.labelCount.Font = new System.Drawing.Font("맑은 고딕", 14F);
+            this.labelCount.Location = new System.Drawing.Point(201, 21);
+            this.labelCount.Name = "labelCount";
+            this.labelCount.Size = new System.Drawing.Size(41, 25);
+            this.labelCount.TabIndex = 33;
+            this.labelCount.Text = "1명";
+            this.labelCount.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Plus_btn
+            // 
+            this.Plus_btn.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Plus_btn.Location = new System.Drawing.Point(254, 15);
+            this.Plus_btn.Name = "Plus_btn";
+            this.Plus_btn.Size = new System.Drawing.Size(44, 34);
+            this.Plus_btn.TabIndex = 32;
+            this.Plus_btn.Text = "+";
+            this.Plus_btn.UseVisualStyleBackColor = false;
+            // 
+            // Minus_btn
+            // 
+            this.Minus_btn.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Minus_btn.Location = new System.Drawing.Point(151, 16);
+            this.Minus_btn.Name = "Minus_btn";
+            this.Minus_btn.Size = new System.Drawing.Size(44, 34);
+            this.Minus_btn.TabIndex = 31;
+            this.Minus_btn.Text = "-";
+            this.Minus_btn.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 14F);
+            this.label1.Location = new System.Drawing.Point(32, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 25);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "총 인원 수 :";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(584, 861);
+            this.Controls.Add(this.ChangePaymentMethod_btn);
+            this.Controls.Add(this.PerPersonAmount);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.CancelPaying_btn);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelCount);
+            this.Controls.Add(this.Plus_btn);
+            this.Controls.Add(this.Minus_btn);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("맑은 고딕", 9F);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button ChangePaymentMethod_btn;
+        private System.Windows.Forms.Label PerPersonAmount;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button CancelPaying_btn;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelCount;
+        private System.Windows.Forms.Button Plus_btn;
+        private System.Windows.Forms.Button Minus_btn;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ KioskProject/KioskProject/UI/PaymentUI.cs
@@ -0,0 +1,133 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace Payment_UI
+{
+    public partial class PaymentUI : Form
+    {
+        private Payment payment = new Payment();
+        private int totalAmount = 5000; //임의 금액으로 5000 값으로 함
+        private int numberOfPeople = 1;
+        
+
+        public PaymentUI()
+        {
+            {
+                InitializeComponent();
+
+                this.Plus_btn.Click += new System.EventHandler(this.Plus_btn_Click); // 인원수 증가 버튼 클릭 이벤트 연결 추가
+                this.Minus_btn.Click += new System.EventHandler(this.Minus_btn_Click); // 인원수 감소 버튼 클릭 이벤트 연결 추가
+            }
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            PerPersonAmount.Text = $"{totalAmount}원"; // 임의 금액으로 5000원으로 표시
+            labelCount.Text = $"{numberOfPeople}명";
+
+            UpdatePersonPanels(); // 처음 로드 시 패널 생성
+        }
+
+        private void Minus_btn_Click(object sender, EventArgs e) // 총인원수 감소 버튼
+        {
+            if (numberOfPeople > 1)
+            {
+                numberOfPeople--;
+                labelCount.Text = $"{numberOfPeople}명";
+                UpdatePersonPanels();
+            }
+        }
+
+        private void Plus_btn_Click(object sender, EventArgs e) // 총인원수 증가 버튼
+        {
+            numberOfPeople++;
+            labelCount.Text = $"{numberOfPeople}명";
+            UpdatePersonPanels();
+        }
+
+        private void CancelPaying_btn_Click(object sender, EventArgs e) // 결제 취소 버튼
+        {
+            payment.CancelPayment(1);
+            MessageBox.Show("결제가 취소되었습니다.");
+
+        }
+
+        private void ChangePaymentMethod_btn_Click(object sender, EventArgs e) //결제 수단 변경 
+        {
+
+        }
+
+        private Panel CreatePersonPanel(int index, int amount)
+        {
+            Panel panel = new Panel
+            {
+                Size = new Size(120, 100),
+                BackColor = Color.White,
+                BorderStyle = BorderStyle.FixedSingle,
+                Margin = new Padding(10)
+            };
+
+            Label lblIndex = new Label
+            {
+                Text = $"{index}",
+                AutoSize = true,
+                Location = new Point(50, 0),
+                Font = new Font("맑은 고딕", 10, FontStyle.Bold)
+            };
+
+            Label lblPrice = new Label
+            {
+                Text = $"{amount}원",
+                AutoSize = true,
+                Location = new Point(30, 30),
+                Font = new Font("맑은 고딕", 12, FontStyle.Bold)
+            };
+
+            Button btnPay = new Button
+            {
+                Text = "결제하기",
+                Enabled = true,
+                Size = new Size(80, 25),
+                Location = new Point(20, 60),
+                BackColor = Color.LightBlue
+            };
+
+            // 버튼 클릭 이벤트 연결
+            btnPay.Click += (sender, e) =>
+            {
+                MessageBox.Show($"{index}번 인원이 {amount}원 결제 합니다.", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            };
+
+            panel.Controls.Add(lblIndex);
+            panel.Controls.Add(lblPrice);
+            panel.Controls.Add(btnPay);
+
+            return panel;
+        }
+
+        // 패널 전체 업데이트
+        private void UpdatePersonPanels()
+        {
+            flowLayoutPanel1.Controls.Clear(); // 기존 패널 제거 
+            int.TryParse(Regex.Replace(PerPersonAmount.Text, @"\D", ""), out int perAmount);
+
+            for (int i = 1; i <= numberOfPeople; i++)
+            {
+                Panel panel = CreatePersonPanel(i, perAmount);
+                flowLayoutPanel1.Controls.Add(panel);
+
+            }
+        }
+    }
+}
