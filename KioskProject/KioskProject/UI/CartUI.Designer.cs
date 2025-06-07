6257a5121048274a2c86c150dbca9f6dd44ca171
허진혁 <point0986@naver.com>
2025-06-07 18:13:18 +0900

    카테고리 ui

---
+++ KioskProject/KioskProject/UI/CartUI.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace KioskProject.UI
+{
+    partial class CartUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Num = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.카테고리 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Num,
+            this.Name,
+            this.Price,
+            this.카테고리});
+            this.dataGridView1.Location = new System.Drawing.Point(11, 108);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(453, 373);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Num
+            // 
+            this.Num.HeaderText = "상품 번호";
+            this.Num.Name = "Num";
+            // 
+            // Name
+            // 
+            this.Name.HeaderText = "상품명";
+            this.Name.Name = "Name";
+            // 
+            // Price
+            // 
+            this.Price.HeaderText = "상품 가격";
+            this.Price.Name = "Price";
+            // 
+            // 카테고리
+            // 
+            this.카테고리.HeaderText = "카테고리";
+            this.카테고리.Name = "카테고리";
+            // 
+            // CartUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 596);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "CartUI";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Num;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 카테고리;
+    }
+}
