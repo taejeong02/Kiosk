895d2b1d5067b464c96525589e86149eccf7ed95
zzun11 <ssj65077@gmail.com>
2025-06-09 11:24:05 +0900

    Revert "Update Program.cs"
    This reverts commit f5e06df77b71df305f749951098d58c65cad4f4f.

--- KioskProject/KioskProject/Program.cs
+++ KioskProject/KioskProject/Program.cs
@@ -17,7 +17,7 @@ namespace KioskProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new SavePointUI(20000));
+            Application.Run(new Select_Language());
         }
     }
 }
