dcd0ac960ed3ca83ca34998515093744d95ecd78
taejeong02 <165948823+taejeong02@users.noreply.github.com>
2025-06-12 01:33:11 +0900

    Update OrderUI.cs

--- KioskProject/KioskProject/UI/OrderUI.cs
+++ KioskProject/KioskProject/UI/OrderUI.cs
@@ -161,8 +161,8 @@ namespace KioskProject
 
                 previousForm2.Show(); // ShopPacking 폼을 직접 표시
 
-                MessageBox.Show("타이머 시간이 만료되었습니다. 메인 화면으로 돌아갑니다.",
-                    "타이머 만료", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+               // MessageBox.Show("타이머 시간이 만료되었습니다. 메인 화면으로 돌아갑니다.",
+               //     "타이머 만료", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 this.Close(); // OrderUI닫기
             }
         }
