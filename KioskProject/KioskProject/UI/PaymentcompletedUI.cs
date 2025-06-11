82916ad83d7d6a65dc48ad92ef923e379b95c034
ghgud04 <shghgud04@naver.com>
2025-06-12 00:49:10 +0900

    Revert "dd"
    This reverts commit dcabbb404744397273d73d5c6f061d831ff6983d.

--- KioskProject/KioskProject/UI/PaymentcompletedUI.cs
+++ KioskProject/KioskProject/UI/PaymentcompletedUI.cs
@@ -99,10 +99,5 @@ namespace KioskProject
             remainingTime = 10;
             Count.Text = $"남은 시간: {remainingTime}초";
         }
-
-        private void PaymentcompletedUI_Load_1(object sender, EventArgs e)
-        {
-
-        }
     }
 }
